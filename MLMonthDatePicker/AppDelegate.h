bf1ef0aea138e14ae88a1809a4ae44dfc9a140bf
molon <dudl@qq.com>
2014-05-26 12:45:51 +0800

    Initial Commit

---
+++ MLMonthDatePicker/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  MLMonthDatePicker
+//
+//  Created by molon on 5/26/14.
+//  Copyright (c) 2014 molon. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
